2ea2e5e9c9788365354c8dfe52d0f0233a601533
AdoLife-creator <order560878@gmail.com>
2026-06-10 17:52:28 +0300

    Update ESP.java

--- src/main/java/com/oxygenclient/module/render/ESP.java
+++ src/main/java/com/oxygenclient/module/render/ESP.java
@@ -2,12 +2,9 @@ package com.oxygenclient.module.render;
 
 import com.oxygenclient.module.Category;
 import com.oxygenclient.module.Module;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.math.Box;
 
 public class ESP extends Module {
     public ESP() {
-        super("ESP", "See players through walls", Category.RENDER);
+        super("ESP", Category.RENDER);
     }
 }
