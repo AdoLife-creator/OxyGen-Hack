4e2a528f3544c96bb0074ab54e549a1ca1670d00
AdoLife-creator <order560878@gmail.com>
2026-06-10 17:51:31 +0300

    Update Sprint.java

--- src/main/java/com/oxygenclient/module/movement/Sprint.java
+++ src/main/java/com/oxygenclient/module/movement/Sprint.java
@@ -5,7 +5,7 @@ import com.oxygenclient.module.Module;
 
 public class Sprint extends Module {
     public Sprint() {
-        super("Sprint", "Auto sprint", Category.MOVEMENT);
+        super("Sprint", Category.MOVEMENT);
     }
 
     @Override
